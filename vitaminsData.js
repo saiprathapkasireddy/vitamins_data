ca3e6d28b77e76f3face5ee34f69a6ecfb21c325
Sai Prathap <saiprathapkasireddy@gmail.com>
2024-02-28 16:28:09 -0500

    data of fruits and their vitamins

---
+++ vitaminsData.js
@@ -0,0 +1,26 @@
+const items = [{
+    name: 'Orange',
+    available: true,
+    contains: "Vitamin C",
+}, {
+    name: 'Mango',
+    available: true,
+    contains: "Vitamin K, Vitamin C",
+}, {
+    name: 'Pineapple',
+    available: true,
+    contains: "Vitamin A",
+}, {
+    name: 'Raspberry',
+    available: false,
+    contains: "Vitamin B, Vitamin A",
+
+}, {
+    name: 'Grapes',
+    contains: "Vitamin D",
+    available: false,
+}];
+
+
+
+module.exports=items;
